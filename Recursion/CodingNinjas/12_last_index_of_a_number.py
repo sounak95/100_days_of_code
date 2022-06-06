4b26616f1c638e41449d59d32e539e9c338a42e7
Sounak Saha <ssounak@vmware.com>
2022-06-07 04:54:59 +0530

    updated

--- Recursion/CodingNinjas/12_last_index_of_a_number.py
+++ Recursion/CodingNinjas/12_last_index_of_a_number.py
@@ -22,6 +22,21 @@ def lastIndexB(a,x, si):
     smallerListOutput= lastIndexB(a,x,si-1)
     return smallerListOutput
 
+def lastIndexC(a,x, si):
+    l=len(a)
+    if si==l:
+        return -1
+    smallerListOutput= lastIndexC(a,x,si+1)
+    if smallerListOutput!=-1:
+        return smallerListOutput
+    else:
+        if a[si]==x:
+            return si
+        else:
+            return -1
+
+
 a = [1, 2, 4, 5, 6, 7, 8, 9, 7]
 print(lastIndex(a, 7))
 print(lastIndexB(a, 7, len(a)-1))
+print(lastIndexC(a, 7,0))
