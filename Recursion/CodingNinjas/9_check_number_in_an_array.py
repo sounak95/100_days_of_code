8d141d369fd1bec408ae8f035e117bf57e670c09
Sounak Saha <ssounak@vmware.com>
2022-06-07 03:13:44 +0530

    updated

---
+++ Recursion/CodingNinjas/9_check_number_in_an_array.py
@@ -0,0 +1,16 @@
+def checkNumber(arr, x):
+    # Please add your code here
+    if len(arr)==1:
+        return arr[0]==x
+    return arr[0]==x or checkNumber(arr[1:],x)
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+x=int(input())
+if checkNumber(arr, x):
+    print('true')
+else:
+    print('false')
