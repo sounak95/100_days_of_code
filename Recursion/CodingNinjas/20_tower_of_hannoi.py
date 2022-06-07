3d31328d58d6373fd2c9e83c5f0cd955e5d6be7a
Sounak Saha <ssounak@vmware.com>
2022-06-08 04:44:02 +0530

    updated

---
+++ Recursion/CodingNinjas/20_tower_of_hannoi.py
@@ -0,0 +1,14 @@
+
+
+def toh(n,source, auxillary, destination):
+    if n==1:
+        print(f"move disk 1 from {source} to {destination}")
+        return
+    toh(n-1,source,auxillary,destination)
+    print(f"mode disk {n} from {source} to {destination}")
+    toh(n-1, auxillary,destination,source)
+
+
+
+n = 4
+toh(n,'A','B','C')
