3f0d020b0751bbbd39c81229b9ab3169e67689c1
Sounak Saha <ssounak@vmware.com>
2022-06-08 11:48:17 +0530

    updated

---
+++ Recursion/CodingNinjas/21_geometric_sun.py
@@ -0,0 +1,36 @@
+'''
+
+Geometric Sum
+Send Feedback
+Given k, find the geometric sum i.e.
+1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k)
+using recursion.
+Input format :
+Integer k
+Output format :
+Geometric sum (upto 5 decimal places)
+Constraints :
+0 <= k <= 1000
+Sample Input 1 :
+3
+Sample Output 1 :
+1.87500
+Sample Input 2 :
+4
+Sample Output 2 :
+1.93750
+Explanation for Sample Input 1:
+1+ 1/(2^1) + 1/(2^2) + 1/(2^3) = 1.87500
+
+
+'''
+
+def geometric_sun(n):
+    if n==0:
+        return 1
+    return 1/pow(2,n) + geometric_sun(n-1)
+
+
+
+print(format(geometric_sun(int(input())),'.5f'))
+
---
+++ Recursion/CodingNinjas/22_check_palindrome.py
@@ -0,0 +1,35 @@
+'''
+
+Check Palindrome (recursive)
+Send Feedback
+Check whether a given String S is a palindrome using recursion. Return true or false.
+Input Format :
+String S
+Output Format :
+'true' or 'false'
+Constraints :
+0 <= |S| <= 1000
+where |S| represents length of string S.
+Sample Input 1 :
+racecar
+Sample Output 1:
+true
+Sample Input 2 :
+ninja
+Sample Output 2:
+false
+
+
+'''
+
+def rev(s,n):
+    if n<0:
+        return ""
+    return s[n]+rev(s,n-1)
+
+str1=input()
+str2=rev(str1, len(str1.strip())-1)
+if str2==str1:
+    print("true")
+else:
+    print("false")
