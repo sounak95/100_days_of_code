0659cd2eca7ae54c35a3c491f07bb73d5f9f54fe
Sounak Saha <ssounak@vmware.com>
2022-06-09 18:42:56 +0530

    updated

---
+++ Arays/patterns/2_container_with_most_water_app_1.py
@@ -0,0 +1,18 @@
+
+import math
+
+def getMaxWaterContainer(heights):
+    max_area= float('-inf')
+    for i, ele in enumerate(heights):
+        for j in range(i+1,len(heights)):
+            height= min(ele, heights[j])
+            width= j-i
+            area= height*width
+            max_area= max(max_area, area)
+    return max_area
+
+
+
+
+heightsArray = [4,8,1,2,3,9];
+print(getMaxWaterContainer(heightsArray))
