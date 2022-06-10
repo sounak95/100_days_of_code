c8319262e9f9ba2ba5957c4efb13c0a53dc60bc9
Sounak Saha <ssounak@vmware.com>
2022-06-10 23:55:43 +0530

    updated

---
+++ Arays/AlgoExpert/easy/5_non_constructable_change.py
@@ -0,0 +1,15 @@
+
+
+def nonConstructibleChange(coins):
+    coins.sort()
+    change_created=0
+    for coin in coins:
+        if coin>change_created+1:
+            return change_created+1
+        change_created+=coin
+    return change_created+1
+
+print(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]))
+
+
+
