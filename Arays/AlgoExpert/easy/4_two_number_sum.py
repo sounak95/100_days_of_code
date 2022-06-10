4824d2674df1a6d85ee6dcc10c3a60ee7891c058
Sounak Saha <ssounak@vmware.com>
2022-06-10 23:13:24 +0530

    updated

---
+++ Arays/AlgoExpert/easy/4_two_number_sum.py
@@ -0,0 +1,15 @@
+def twoNumberSum(array, targetSum):
+    array.sort()
+    left=0
+    right=len(array)-1
+    while(left<right):
+        currentSum=array[left]+array[right]
+        if currentSum==targetSum:
+            return [array[left], array[right]]
+        if currentSum<targetSum:
+            left+=1
+        if currentSum>targetSum:
+            right-=1
+    return []
+
+print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
