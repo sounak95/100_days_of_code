33e27e894b0f0911c3775cf1562113e750c16c3c
Sounak Saha <ssounak@vmware.com>
2022-06-11 03:25:12 +0530

    updated

---
+++ Arays/AlgoExpert/medium/4_monotonic_array.py
@@ -0,0 +1,22 @@
+
+def breakDirection(direction, currentElement, prevElement):
+    diff=currentElement-prevElement
+    if direction>0:
+        return diff<0
+    return diff>0
+
+def isMonotonic(array):
+    # Write your code here.
+    if len(array)<=2:
+        return True
+    direction=array[1]-array[0]
+    for i in range(2,len(array)):
+        if direction==0:
+            direction=array[i]-array[i-1]
+            continue
+        if breakDirection(direction, array[i], array[i-1]):
+            return False
+    return True
+
+print(isMonotonic( [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]))
+
