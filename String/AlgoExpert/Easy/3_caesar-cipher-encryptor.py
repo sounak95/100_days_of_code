797a1877b182199a69e57e0d05ffbbdebddc92de
Sounak Saha <ssounak@vmware.com>
2022-06-13 21:41:43 +0530

    updated

--- String/AlgoExpert/Easy/3_caesar-cipher-encryptor.py
+++ String/AlgoExpert/Easy/3_caesar-cipher-encryptor.py
@@ -1,13 +1,20 @@
 
 def getNewLetter(letter, key):
+    key=key%26
     newLettercode=ord(letter)+key
     return chr(newLettercode) if newLettercode<=122 else chr(96+newLettercode%122)
+
+def getNewLetter1(letter, key):
+    key=key%26
+    alphabet="abcdefghijklmnopqrstuvwxyz"
+    newLettercode=alphabet.index(letter)+key
+    return alphabet[newLettercode%26]
 
 def caesarCipherEncryptor(string, key):
     # Write your code here.
     str1=''
     for ch in string:
-        str1 += getNewLetter(ch,key)
+        str1 += getNewLetter1(ch,key)
     return str1
 
 print(caesarCipherEncryptor("xyz",2))
