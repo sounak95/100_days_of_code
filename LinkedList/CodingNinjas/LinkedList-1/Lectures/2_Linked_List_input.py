76076de862eade83b3f52b57ecd87d3fb14f5722
Sounak Saha <ssounak@vmware.com>
2022-07-06 20:12:56 +0530

    updated

--- LinkedList/CodingNinjas/LinkedList-1/Lectures/2_Linked_List_input.py
+++ LinkedList/CodingNinjas/LinkedList-1/Lectures/2_Linked_List_input.py
@@ -4,6 +4,15 @@ class Node:
         self.data=data
         self.next=None
 
+
+def print_ll(head):
+    while head is not None:
+        print(str(head.data) + "->", end='')
+        head=head.next
+    print(None)
+    return
+
+
 def takeInput():
     inputList=[int(ele) for ele in input().split()]
     head=None
@@ -20,6 +29,23 @@ def takeInput():
             curr.next= newNode
     return head
 
+def takeInput1():
+    inputList=[int(ele) for ele in input().split()]
+    head=None
+    tail=None
+    for currentData in inputList:
+        if currentData==-1:
+            break
+        newNode= Node(currentData)
+        if head is None:
+            head=newNode
+            tail=newNode
+        else:
+            tail.next=newNode
+            tail=newNode
+    return head
 
-head=takeInput()
-print(head)
+
+head=takeInput1()
+# print(head)
+print_ll(head)
