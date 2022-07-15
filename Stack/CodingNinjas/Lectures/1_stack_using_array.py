c3c14517465d3755da304e8a06162aa7c95f3fd9
Sounak Saha <ssounak@vmware.com>
2022-07-15 05:33:22 +0530

    updated

---
+++ Stack/CodingNinjas/Lectures/1_stack_using_array.py
@@ -0,0 +1,33 @@
+
+class Stack:
+    def __init__(self):
+        self.__data=[]
+
+    def push(self, item):
+        self.__data.append(item)
+
+    def pop(self):
+        if self.isEmpty():
+            print("Stack is empty")
+            return
+        return self.__data.pop()
+
+    def top(self):
+        if self.isEmpty():
+            print("Stack is empty")
+            return
+        return self.__data[len(self.__data)-1]
+
+    def size(self):
+        return len(self.__data)
+
+    def isEmpty(self):
+        return self.size()==0
+
+s=Stack()
+s.push(12)
+s.push(13)
+s.push(15)
+while s.isEmpty() is False:
+    print(s.pop())
+s.top()
