5fbc0493e936d4426f50d13106ea834fe299f768
Sounak Saha <ssounak@vmware.com>
2022-07-19 21:52:38 +0530

    updated

--- Queue/CodingNinjas/Lectures/4_Stack Using 2 Queues.py
+++ Queue/CodingNinjas/Lectures/4_Stack Using 2 Queues.py
@@ -1,28 +1,63 @@
 from sys import stdin
 
+class QueueUsingArray:
+    def __init__(self):
+        self.__arr=[]
+        self.__count=0
+        self.__front=0
+
+    def size(self):
+        return self.__count
+
+    def isEmpty(self):
+        return self.size()==0
+
+    def enqueue(self, data):
+        self.__arr.append(data)
+        self.__count+=1
+
+    def dequeue(self):
+        if self.isEmpty():
+            return -1
+        element= self.__arr[self.__front]
+        self.__front+=1
+        self.__count-=1
+        return element
+
+    def front(self):
+        if self.isEmpty():
+            return -1
+        return self.__arr[self.__front]
 
 class Stack:
     # Define data members and __init__()
-
+    def __init__(self):
+        self.__q1 = QueueUsingArray()
+        self.__q2 = QueueUsingArray()
     '''----------------- Public Functions of Stack -----------------'''
 
     def getSize(self):
-        
+        return self.__q1.size()
     # Implement the getSize() function
 
     def isEmpty(self):
-
+        return self.__q1.isEmpty()
     # Implement the isEmpty() function
 
     def push(self, data):
-
+        self.__q2.enqueue(data)
+        while(not self.__q1.isEmpty()):
+            data = self.__q1.front()
+            self.__q2.enqueue(self.__q1.dequeue())
+        self.__q1, self.__q2= self.__q2, self.__q1
     # Implement the push(element) function
 
     def pop(self):
-
+        return self.__q1.dequeue()
     # Implement the pop() function
 
     def top(self):
+        return self.__q1.front()
 
 
 # Implement the top() function
--- Stack/CodingNinjas/Lectures/3_Inbuillt Stacks & Queues.py
+++ Stack/CodingNinjas/Lectures/3_Inbuillt Stacks & Queues.py
@@ -15,7 +15,7 @@ q.put(1)
 q.put(2)
 q.put(3)
 q.put(4)
-
+print(f"size: {q.qsize()}")
 while not q.empty():
     print(q.get())
 
