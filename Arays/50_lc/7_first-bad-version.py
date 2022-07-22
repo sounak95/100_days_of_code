4524a0634114dc285ff28d30aa6c1149a3ecc4c2
sounak95@gmail.com <ssounak@vmware.com>
2022-07-23 02:26:24 +0530

    updated

--- Arays/50_lc/7_first-bad-version.py
+++ Arays/50_lc/7_first-bad-version.py
@@ -3,9 +3,24 @@
 # @return a bool
 # def isBadVersion(version):
 # https://leetcode.com/problems/first-bad-version/
+
+
 class Solution(object):
+    def isBadVersion(self, version):
+        pass
     def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
+        left =1
+        right = n
+
+        while(left<right):
+            mid = (left+right)//2
+            if self.isBadVersion(mid):
+                right=mid
+            else:
+                left=mid+1
+        return left
+
