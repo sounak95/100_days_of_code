d327be799ad871b18f1c1f885209885bbc5ea2f9
Sounak Saha <ssounak@vmware.com>
2022-07-23 01:51:54 +0530

    updated

---
+++ Arays/50_lc/7_first-bad-version.py
@@ -0,0 +1,11 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+# https://leetcode.com/problems/first-bad-version/
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
