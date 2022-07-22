135696f0ec3c00444c3678d3da08f7d1b900775c
Sounak Saha <ssounak@vmware.com>
2022-07-22 21:50:28 +0530

    updated

--- Arays/50_lc/5_longest-substring-without-repeating-characters.py
+++ Arays/50_lc/5_longest-substring-without-repeating-characters.py
@@ -1,5 +1,31 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
+
+def lengthOfLongestSubstring_optimal(s):
+    n= len(s)
+    left=right=0
+    m={}
+    ans =0
+    substr=""
+    while(left<n and right<n):
+        el=s[right]
+        if el in m:
+            left=max(left, m[el]+1)
+        m[el]=right
+
+        substr1 = s[left:right+1]
+        len1 = right-left+1
+        if len1>ans:
+            ans=len1
+            substr=substr1
+        right += 1
+
+        # ans= max(ans, right-left+1)
+    return ans
+
+
+
+# brute force
 def lengthOfLongestSubstring(s):
     """
     :type s: str
@@ -21,11 +47,14 @@ def lengthOfLongestSubstring(s):
                 max_length=current_length
                 longest_substr=substr
 
-    # print(longest_substr)
+    print(longest_substr)
     return max_length
 
 print(lengthOfLongestSubstring("abcabcbb"))
-print(lengthOfLongestSubstring(" "))
-print(lengthOfLongestSubstring(""))
+# print(lengthOfLongestSubstring(" "))
+# print(lengthOfLongestSubstring(""))
+print(lengthOfLongestSubstring_optimal("abcabcbb"))
+# print(lengthOfLongestSubstring_optimal(" "))
+# print(lengthOfLongestSubstring_optimal(""))
 
 
