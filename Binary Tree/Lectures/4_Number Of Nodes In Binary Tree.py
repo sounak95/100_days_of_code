daf886bb106e5e21babbd22d74e1f4338dc49999
sounak95 <ssounak@vmware.com>
2022-07-25 15:42:32 +0530

    updated

---
+++ Binary Tree/Lectures/4_Number Of Nodes In Binary Tree.py
@@ -0,0 +1,48 @@
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data= data
+        self.left = None
+        self.right = None
+
+def printTree(root):
+    if root ==None:
+        return
+    print(root.data)
+    printTree(root.left)
+    printTree(root.right)
+
+
+
+def printDetailedTree(root):
+    if root ==None:
+        return
+    print(root.data , end=":")
+    if root.left !=None:
+        print(f"L: {root.left.data}", end=',')
+    if root.right!=None:
+        print(f"R: {root.right.data}", end='')
+    print()
+    printDetailedTree(root.left)
+    printDetailedTree(root.right)
+
+def treeInput():
+    rootData= int(input())
+    if rootData==-1:
+        return None
+    root = BinaryTreeNode(rootData)
+    leftTree=treeInput()
+    rightTree=treeInput()
+    root.left = leftTree
+    root.right = rightTree
+    return root
+
+def numNodes(root):
+    if root ==None:
+        return 0
+    leftCount= numNodes(root.left)
+    rightCount = numNodes(root.right)
+    return leftCount+rightCount+1
+
+root=treeInput()
+printDetailedTree(root)
+print(numNodes(root))
