e3304bf8a370216ca92c96a1dbbf2289b1843d6f
sounak95 <ssounak@vmware.com>
2022-07-29 06:54:23 +0530

    updated

---
+++ String/GeeksForGeeks/4_Recursively remove all adjacent duplicates.py
@@ -0,0 +1,26 @@
+
+# https://practice.geeksforgeeks.org/problems/recursively-remove-all-adjacent-duplicates0744/1
+
+def rremove(s):
+    temp=""
+    while(temp!=s):
+        temp=s
+        s=remove(s)
+    return temp
+
+
+def remove(s):
+    i=0
+    n=len(s)
+    res=""
+    while(i<n):
+        if i<n-1 and s[i]==s[i+1]:
+            while(i<n-1 and s[i]==s[i+1]):
+                i+=1
+        else:
+            res+=s[i]
+        i+=1
+
+    return res
+
+print(rremove("geeksforgeek"))
