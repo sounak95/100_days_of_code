82e1eaf58c8bf9db6552074170eef5bc9f4ddc48
sounak95 <ssounak@vmware.com>
2022-09-03 05:43:02 +0530

    updated

--- LinkedList/Neetcode/11_Reverse Nodes in k-Group.py
+++ LinkedList/Neetcode/11_Reverse Nodes in k-Group.py
@@ -14,4 +14,33 @@ class Solution(object):
         :type k: int
         :rtype: ListNode
         """
+        dummy = ListNode(0,head)
+        groupPrev = dummy
+
+        while True:
+            kth = self.getKthNode(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+
+            prev, curr = kth.next, groupPrev.next
+
+            while curr !=groupNext:
+                next = curr.next
+                curr.next=prev
+                prev=curr
+                curr=next
+
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev= tmp
+
+        return dummy.next
+
+
+    def getKthNode(self, curr, k):
+        while curr and k>0:
+            curr= curr.next
+            k-=1
+        return curr
 
